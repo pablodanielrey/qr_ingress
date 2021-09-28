edb20b6fa570c39b610026c8c5e2f305ffdd773f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-27 21:51:45 -0300

    ya estoy generando qrs en formatos estandares

--- qr/quiz/urls.py
+++ qr/quiz/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accepted)
+    path('', views.accepted),
+    path('qr/<str:user_id>', views.qr_code_view)
 ]
--- qr/quiz/views.py
+++ qr/quiz/views.py
@@ -4,6 +4,11 @@ from django.http import JsonResponse, HttpResponse
 
 from .models import QuizAttempt, QuizGrade, User
 
+#https://medium.com/geekculture/how-to-generate-a-qr-code-in-django-e32179d7fdf2
+import qrcode
+import qrcode.image.svg
+from io import BytesIO
+
 # Create your views here.
 
 def accepted(request):
@@ -18,3 +23,12 @@ def accepted(request):
     ]
     print(r)
     return JsonResponse({'status':200,'data':r}, json_dumps_params={"ensure_ascii": False})
+
+
+def qr_code_view(request, user_id):
+    factory = qrcode.image.svg.SvgImage
+    image = qrcode.make('MECARD:N:Mariano Visentin;TEL:+54 9 11 6767-0165;;', image_factory=factory, box_size=20)
+    stream = BytesIO()
+    image.save(stream)
+    data = stream.getvalue().decode()
+    return HttpResponse(data, content_type='image/svg+xml')
