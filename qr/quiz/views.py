ccb9b644e4ef19d29caab375173711d139db0c21
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-27 21:22:25 -0300

    agregue una api de ejemplo para obtener los datos

--- qr/quiz/views.py
+++ qr/quiz/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+# from django.core import serializers
+
+from .models import QuizAttempt, QuizGrade, User
 
 # Create your views here.
+
+def accepted(request):
+
+    data = QuizGrade.objects.all()
+    # jdata = serializers.serialize('json',data)
+    r = [
+        {
+            'idnumber':d.userid.idnumber,
+            'grade': d.grade
+        } for d in data
+    ]
+    print(r)
+    return JsonResponse({'status':200,'data':r}, json_dumps_params={"ensure_ascii": False})
