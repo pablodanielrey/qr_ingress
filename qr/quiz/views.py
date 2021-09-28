e9fc0e683bd11abc1ff9d481028838837ef759da
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-28 17:29:42 -0300

    corregi algunas cosas para hacerlo mas robusto. cuando se ingresa un id de usuario que no existe

--- qr/quiz/views.py
+++ qr/quiz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from django.db import models
+
 # from django.core import serializers
 
 from datetime import datetime, timezone
@@ -50,9 +52,12 @@ def _get_enabled_quiz(user_id):
         return None
     """
 
-    user = User.objects.get(id=user_id)
-    grades = user.grades.filter(quiz=7168, grade=10.0)
-    return user, grades
+    try:
+        user = User.objects.get(id=user_id)
+        grades = user.grades.filter(quiz=7168, grade=10.0)
+        return user, grades
+    except User.DoesNotExist as e:
+        return None, None
     #return QuizGrade.objects.filter(timemodified__gt=week_before_t, userid__id=user_id, grade=10.0)
     #return QuizGrade.objects.filter(quiz=7168, user__id=user_id, grade=10.0)
 
@@ -69,25 +74,25 @@ def qr_code_view(request, user_id):
 
 
     user, quizs = _get_enabled_quiz(user_id)
-    quiz = quizs.first()
-    if not quiz:
-        print('No hay resultados')
-        qr.add_data(f"Persona: {user.firstname} {user.lastname};")
-        qr.add_data('Inválido')
-        qr.add_data(uuid.uuid4())
+
+    
+    if not user:
+        data = f"Inválido;{uuid.uuid4()}"
     else:
-        print(quiz)
-        qr.add_data(f"Persona: {quiz.user.firstname} {quiz.user.lastname};")
-        qr.add_data(f"Calificación: {quiz.grade}")
-        qr.add_data(uuid.uuid4())
-        
+        quiz = quizs.first()
+        if not quiz:
+            data = f"Persona: {user.firstname} {user.lastname};Inválido;{uuid.uuid4()}"
+        else:
+            data = f"Persona: {quiz.user.firstname} {quiz.user.lastname};Calificación: {quiz.grade};{uuid.uuid4()}"
+
     #qr.add_data(f"MECARD:N:{user.firstname} {user.lastname};TEL:+54 9 221 3033138;;")
     #qr.add_data('https://youtu.be/YqsdmQsjQds')
     #qr.add_data(f"WIFI:T:WPA;S:mynetwork;P:mypass;;")
     # qr.add_data('http://localhost:8000/quiz/')
     # qr.add_data('MECARD:N:Mariano Visentin;TEL:+54 9 11 6767-0165;;')
     # qr.add_data('mailto:mariano.visentin@econo.unlp.edu.ar')
-    
+    qr.add_data(data)
+
     image = qr.make_image()
     stream = BytesIO()
     image.save(stream)
