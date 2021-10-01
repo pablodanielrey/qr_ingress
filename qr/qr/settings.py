b4186498fbe04c60917229852e613eb4e2dff52c
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-30 22:01:34 -0300

    actualice a la ultima version de las configs para que las saque del environment

--- qr/qr/settings.py
+++ qr/qr/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,7 +75,6 @@ WSGI_APPLICATION = 'qr.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-import os
 
 DATABASE_ROUTERS = ['quiz.models.MoodleRouter']
 
