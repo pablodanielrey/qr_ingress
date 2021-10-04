57d7bd4a2ad8e39cd54f67f5f4abaea1d28993c0
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-04 19:12:01 -0300

    agregue un chequeo adicional que indica cuando el codigo qr es invalido

--- qr_client/reader/views.py
+++ qr_client/reader/views.py
@@ -15,14 +15,31 @@ def _get_hmac_signature(data):
     h = hmac.new(bs, bytes(data,'utf-8'), hashlib.sha1).hexdigest()
     return h
 
-def _verify_hmac(data):
+def _decode_qr(data):
     sdata = data.split(';')
     h = sdata[-1]
     original_data = data.replace(f"{h}",'')
     newh = _get_hmac_signature(original_data)
     
-    logging.debug(f"data:{data}\nhash:{h}\noriginal:{original_data}\nnew h:{newh}")
-    return h == newh
+    decoded = {
+        'data': data,
+        'original_hash': h,
+        'computed_hash': newh
+    }
+    for field in sdata:
+        field_data = field.split(':')
+        try:
+            decoded[field_data[0]] = field_data[1]
+        except IndexError as e:
+            logging.warn(data, e)
+
+    logging.debug(decoded)
+    return decoded
+
+def _verify_hmac(decoded):
+    decoded['original_hash'] == decoded['computed_hash']
+
+
 
 
 def index(request:HttpRequest):
@@ -32,13 +49,19 @@ def index(request:HttpRequest):
     if request.method == 'POST':
         qrcode = request.POST['qrcode']
         logging.debug(f'QRCODE: {qrcode}')
-        if _verify_hmac(qrcode):
-            return redirect('reader:access_authorized')
+        decoded = _decode_qr(qrcode)
+        if not _verify_hmac(decoded):
+            return redirect('reader:invalid_qr')
+        
+
 
     return redirect('reader:access_denied') 
 
 def authorized(request):
-    return render(request, 'authorized.html')
+    return render(request, 'authorized.html', {'timer':2})
 
 def denied(request):
-    return render(request, 'denied.html')
+    return render(request, 'denied.html', {'timer':2})
+
+def invalid(request):
+    return render(request, 'invalid.html', {'timer':2})
