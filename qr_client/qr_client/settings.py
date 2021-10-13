4245041f18ea21754741d33eb52c17074925dbcc
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-13 14:47:01 -0300

    agrego las settings

--- qr_client/qr_client/settings.py
+++ qr_client/qr_client/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c23znx&9fok*_va6+noy_ugo6&#pwyjpi)mc6ui^9o^v#ns&_c'
+SECRET_KEY = os.environ.get('DJANGO_SECRET','django-insecure-d=ko^dojcz=nxtwx^o24)@iqc#@!!m$j1em8iwda5=xp-@02qo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG','0') == '1' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','*').split(';')
 
 
 # Application definition
