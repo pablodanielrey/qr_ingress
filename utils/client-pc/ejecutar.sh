a3dc1567ea64ebe5be08d85d2af12705436b1c92
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-14 16:49:47 -0300

    actualice las pantallas y agregue los scripts que usamos en el cliente

---
+++ utils/client-pc/ejecutar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -R /home/qr/.config/google-chrome
+mkdir -p /home/qr/.config/google-chrome
+touch /home/qr/.config/google-chrome/First\ Run
+READY=`curl -sL -w "%{http_code}" "http://10.107.73.172/" -o /dev/null`
+while [ "$READY" != "200" ];
+do
+	sleep 10s;
+	READY=`curl -sL -w "%{http_code}" "http://10.107.73.172/" -o /dev/null`
+done
+/usr/bin/i3
