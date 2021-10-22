3afbe86b9eeef85d0a6071fd79da74f3bbc5bd59
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-22 14:20:56 -0300

    agegamos para que elimine el cache

--- utils/client-pc/ejecutar.sh
+++ utils/client-pc/ejecutar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -R /home/qr/.config/google-chrome
+rm -R /home/qr/.cache/google-chrome
 mkdir -p /home/qr/.config/google-chrome
 touch /home/qr/.config/google-chrome/First\ Run
 READY=`curl -sL -w "%{http_code}" "http://10.107.73.172/" -o /dev/null`
